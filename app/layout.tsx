0fcc65715ffb0e718a3e0fda03967ccd6f84e008
Mayankjg <24bmiitmscit005@gmail.com>
2025-12-25 10:23:13 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,14 @@ export default function RootLayout({
 }
 
 
+
+
+
+
+
+
+
+
 // import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
