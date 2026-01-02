28dea2e1b3788f67cfa3c2addd430f8359ff08d5
Mayankjg <24bmiitmscit005@gmail.com>
2026-01-02 14:13:00 +0530

    commit

--- app/newsletter/SendMail/CustomMessage/page.js
+++ app/newsletter/SendMail/CustomMessage/page.js
@@ -321,7 +321,7 @@ export default function CustomMessage() {
               id="product-select-custom"
               value={selectedProduct}
               onChange={(e) => setSelectedProduct(e.target.value)}
-              className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm text-gray-700"
+              className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none hover:bg-gray-100 text-sm text-gray-700"
             >
               <option value="">Select Products</option>
               {products.map((product) => (
@@ -348,7 +348,7 @@ export default function CustomMessage() {
                 id="email-select-custom"
                 value={selectedEmail}
                 onChange={(e) => setSelectedEmail(e.target.value)}
-                className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm text-gray-700"
+                className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none hover:bg-gray-100 text-sm text-gray-700"
               >
                 <option value="">Select Email</option>
                 <option value="mayank@gmail.com">mayank@gmail.com</option>
@@ -413,7 +413,7 @@ export default function CustomMessage() {
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
               placeholder="Subject"
-              className="shadow appearance-none border border-gray-300 rounded w-full sm:max-w-md sm:flex-1 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-gray-50 text-sm"
+              className="shadow appearance-none border border-gray-300 rounded w-full sm:max-w-md sm:flex-1 py-2 px-4 text-gray-700 leading-tight focus:outline-none hover:bg-gray-100 text-sm"
             />
           </div>
         </div>
