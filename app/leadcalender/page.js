bba0ed5aa37867ca04a86dca209f04aeac5005bd
Mayankjg <24bmiitmscit005@gmail.com>
2026-01-10 11:39:21 +0530

    commit

--- app/leadcalender/page.js
+++ app/leadcalender/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ArrowBigRight, ArrowBigLeft } from 'lucide-react';
 
 const LeadsCalendar = () => {
